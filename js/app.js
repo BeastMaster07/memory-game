ff7d0c4881b2f6ab6c2c51ac53fb3f6b2aecee5e
Aman <agarwalaman6397@gmail.com>
2018-07-18 18:27:37 +0530

    Removed unnecessary files

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
-var card, cardCount, moves, moveText, moveCount, movesTaken, timeTaken, stars, card_1, ul, cardOpenCount, flag, afterText, playAgain_yes, playAgain_no, afterText_moves, afterText_playAgain, timer, flag_timer, sec, min, hrs, t, storage;
-
+var card, cardCount, moves, moveText, moveCount, movesTaken, timeTaken, stars, card_1, ul, cardOpenCount, flag, afterText, playAgain_yes, playAgain_no, afterText_moves, afterText_playAgain, timer, flag_timer, sec, min, hrs, t;
 
 card = document.getElementsByClassName('card');
 moves = document.getElementsByClassName('moves');
@@ -17,100 +16,92 @@ timer = document.getElementsByClassName('time')[0];
 
 reset();
 
-// storage = [];
-
-
-for(var i = 0; i < 16; i++) {
-   card[i].addEventListener('click', function() {
-       if(flag_timer) {
-        if(sec === 0 && min === 0 && hrs === 0) {
-            t = setInterval(add, 1000);
+for (var i = 0; i < 16; i++) {
+    card[i].addEventListener('click', function () {
+        if (flag_timer) {
+            if (sec === 0 && min === 0 && hrs === 0) {
+                t = setInterval(add, 1000);
+            }
+            flag_timer = false;
         }
-        flag_timer = false;
-       }
 
-       if(flag) {
-        cardOpen(this);
-       }
-  });
-      }  
+        if (flag) {
+            cardOpen(this);
+        }
+    });
+}
 
-ul[0].addEventListener('click', function() {
-    clearInterval(t);
-    reset();
-    timer.textContent = (hrs ? (hrs > 9 ? hrs : "0" + hrs) : "00") + ":" + (min ? (min > 9 ? min : "0" + min) : "00") + ":" + (sec > 9 ? sec : "0" + sec);
-
-});
-
-playAgain_yes.addEventListener('click', function() {
+ul[0].addEventListener('click', function () {
     clearInterval(t);
     reset();
     timer.textContent = (hrs ? (hrs > 9 ? hrs : "0" + hrs) : "00") + ":" + (min ? (min > 9 ? min : "0" + min) : "00") + ":" + (sec > 9 ? sec : "0" + sec);
 });
 
-playAgain_no.addEventListener('click', function() {
+playAgain_yes.addEventListener('click', function () {
+    clearInterval(t);
+    reset();
+    timer.textContent = (hrs ? (hrs > 9 ? hrs : "0" + hrs) : "00") + ":" + (min ? (min > 9 ? min : "0" + min) : "00") + ":" + (sec > 9 ? sec : "0" + sec);
+});
+
+playAgain_no.addEventListener('click', function () {
     afterText_moves.style.display = "none";
     afterText_playAgain.style.display = "none";
 });
 
-function cardOpen(index) {   
-  if(!(index.classList.contains('open'))) {
-      if(cardOpenCount == 0) {
-    card_1 = index;
-    card_1.classList.add('open');
-    cardOpenCount++;
-  } else if(cardOpenCount == 1) {
-    moves[0].textContent = ++moveCount;
-    cardOpenCount = 0;
-    flag = false;
-    index.classList.add('open');
-    
-    if(card_1.isEqualNode(index)) {
-          cardCount -= 2;
-      setTimeout(function () {
-        index.classList.add("match");
-        card_1.classList.add("match");
-        flag = true;
-      }, 500);
-    } else {
-      setTimeout(function () {
-      index.classList.remove("open");
-      card_1.classList.remove("open");
-      index.classList.toggle("shake");
-      card_1.classList.toggle("shake");
-      flag = true;
-      }, 1000);
-      index.classList.toggle("shake");
-      card_1.classList.toggle("shake");
+function cardOpen(index) {
+    if (!(index.classList.contains('open'))) {
+        if (cardOpenCount == 0) {
+            card_1 = index;
+            card_1.classList.add('open');
+            cardOpenCount++;
+        } else if (cardOpenCount == 1) {
+            moves[0].textContent = ++moveCount;
+            cardOpenCount = 0;
+            flag = false;
+            index.classList.add('open');
 
+            if (card_1.isEqualNode(index)) {
+                cardCount -= 2;
+                setTimeout(function () {
+                    index.classList.add("match");
+                    card_1.classList.add("match");
+                    flag = true;
+                }, 500);
+            } else {
+                setTimeout(function () {
+                    index.classList.remove("open");
+                    card_1.classList.remove("open");
+                    index.classList.toggle("shake");
+                    card_1.classList.toggle("shake");
+                    flag = true;
+                }, 1000);
+                index.classList.toggle("shake");
+                card_1.classList.toggle("shake");
+
+            }
+            if (cardCount == 0) {
+                clearInterval(t);
+                setTimeout(function () {
+                    afterText.style.display = "";
+                    movesTaken.textContent = moveCount;
+                    timeTaken.textContent = timer.textContent;
+                    if (moveCount >= 16) {
+                        stars[4].style.display = "none";
+                        stars[5].style.display = "none";
+                    } else if (moveCount >= 12) {
+                        stars[5].style.display = "none";
+                    }
+                }, 500);
+            }
+        }
     }
-    if(cardCount == 0) {
-        // localStorage.setItem('moves', moveCount);
-        // storage.push(localStorage.getItem('moves'));
-        // console.log(storage);
-        clearInterval(t);
-        setTimeout(function () {
-            afterText.style.display = "";
-            movesTaken.textContent = moveCount;
-            timeTaken.textContent = timer.textContent;
-            if(moveCount >= 16) {
-                stars[4].style.display = "none";
-                stars[5].style.display = "none";
-              } else if(moveCount >= 12) {
-                stars[5].style.display = "none";
-              }
-        }, 500);
 
+    if (moveCount >= 16) {
+        stars[1].style.display = "none";
+    } else if (moveCount >= 12) {
+        stars[2].style.display = "none";
     }
-  }
-  } 
 
-   if(moveCount >= 16) {
-    stars[1].style.display = "none";
-  } else if(moveCount >= 12) {
-    stars[2].style.display = "none";
-  }
-  
 }
 
 
@@ -127,24 +118,24 @@ function init() {
 
     flag = true;
     flag_timer = true;
-  moveCount = 0;
-  moves[0].textContent = 0;
-  cardOpenCount = 0;
-  cardCount = card.length;
-  afterText.style.display = "none";
-  for(var i = 0; i < card.length; i++) {
-    card[i].classList.remove('open', 'match');
-  }
-  for(var i = 1; i < 3; i++) {
-    stars[i].style.display = "";
-  }
+    moveCount = 0;
+    moves[0].textContent = 0;
+    cardOpenCount = 0;
+    cardCount = card.length;
+    afterText.style.display = "none";
+    for (var i = 0; i < card.length; i++) {
+        card[i].classList.remove('open', 'match');
+    }
+    for (var i = 1; i < 3; i++) {
+        stars[i].style.display = "";
+    }
 }
 
-function shuffle () {
+function shuffle() {
     var ul = document.getElementsByClassName('deck');
     for (var i = ul[0].children.length; i >= 0; i--) {
-     ul[0].appendChild(ul[0].children[Math.random() * i | 0]);
-}
+        ul[0].appendChild(ul[0].children[Math.random() * i | 0]);
+    }
 }
 
 
@@ -158,8 +149,5 @@ function add() {
             hrs++;
         }
     }
-    
     timer.textContent = (hrs ? (hrs > 9 ? hrs : "0" + hrs) : "00") + ":" + (min ? (min > 9 ? min : "0" + min) : "00") + ":" + (sec > 9 ? sec : "0" + sec);
-
 }
-
